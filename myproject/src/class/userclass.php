44ac9285617b5db7fd675c8a2c44441b00382651
gavrilovaa82@rambler.ru <gavrilovaa82@rambler.ru>
2017-04-06 21:55:01 +0700

    project

--- myproject/src/class/userclass.php
+++ myproject/src/class/userclass.php
@@ -13,7 +13,7 @@ class user
 		var_dump(['file' => $file, 'describe' => $describe, 'ID_user' => $ID_user, 'date1' => $date1, 'category' => $category]);
 		//var_dump($category);
 		//$this->dbconnection->query("SET NAMES utf8");
-		$sql = "INSERT INTO foto (`url`, `describe`, `ID_user`, `DATE`, `category`) values (:file, :describe, :ID_user, :date1, :category)";
+		$sql = "INSERT INTO foto1 (`url`, `describe`, `ID_user`, `DATE`, `category`) values (:file, :describe, :ID_user, :date1, :category)";
 		$statement = $this->dbconnection->prepare($sql);
 		$statement->bindParam(':ID_user', $ID_user, PDO::PARAM_INT);
 		$statement->bindParam(':file', $file, PDO::PARAM_STR);
